e3f0a26caba913d0479bc898dd634ebd6be1834a
Alex Francois <apfrancois86@gmail.com>
2017-01-20 08:59:12 -0800

    add initial file structure

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { FormsModule }  from '@angular/forms';
+import { FoodListComponent }  from './food-list.component';
+import { EditFoodComponent } from './edit-food.component';
+import { NewFoodComponent } from './new-food.component';
+import { CaloriePipe } from './calorie.pipe';
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, FoodListComponent, EditFoodComponent, NewFoodComponent, CaloriePipe ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
---
+++ app/food-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model';
+
+@Component({
+  
+})
+
+export class FoodListComponent {
+
+}
