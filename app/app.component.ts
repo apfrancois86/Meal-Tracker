18041054d389ceb6867cad0c9c2386d1d5e28dbf
Alex Francois <apfrancois86@gmail.com>
2017-01-20 09:36:55 -0800

    add food-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Food } from './food.model';
   <div class="container">
     <h1>Meal Tracker for {{month}}/{{day}}/{{year}}</h1>
     <hr>
-
+    <food-list [childFoodList]="masterFoodList"></food-list>
   </div>
   `
 })
@@ -17,4 +17,10 @@ export class AppComponent {
   month: number = this.currentTime.getMonth()+1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear()
+  selectedFood = null;
+  masterFoodList: Food[] = [
+    new Food('Steak','delicious filet mignon', 600),
+    new Food('Cheesecake', 'I ate two pieces!', 850),
+    new Food('Green Salad','fresh and healthy', 300)
+  ];
 }
--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -2,9 +2,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Food } from './food.model';
 
 @Component({
-  
+  selector: 'food-list',
+  template: `
+
+      <div *ngFor="let currentFood of childFoodList">
+      <h2>{{currentFood.food}}
+      <ul>
+      <li>Description: {{currentFood.description}}</li>
+      <li>Calories: {{currentFood.calories}}</li>
+      </ul>
+  `
 })
 
 export class FoodListComponent {
-
+  @Input() childFoodList: Food[];
 }
